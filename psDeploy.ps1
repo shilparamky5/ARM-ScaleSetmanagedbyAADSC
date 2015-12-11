e516b2762c0f1d3279973347eae4be1a74f7894c
Michael Greene <migreene@outlook.com>
2015-12-11 11:58:50 -0600

    add increment counter as param

--- psDeploy.ps1
+++ psDeploy.ps1
@@ -3,11 +3,14 @@
 #  This script can be used to test the ARM template deployment, or as a reference for building your own deployment script.
 param (
 	[Parameter(Mandatory=$true)]
+	[int]$i,
+	[Parameter(Mandatory=$true)]
 	[pscredential]$Credential 
 )  
-$ResourceGroupName = 'vmss20'
-$AccountName = 'AutomationAccount20'
+$ResourceGroupName = "vmss$i"
+$AccountName = "AutomationAccount$i"
+$DomainNameLabel = "demoapp$i"
 New-AzureRmResourcegroup -Name $ResourceGroupName -Location 'East US' -Verbose
 New-AzureRMAutomationAccount -ResourceGroupName $ResourceGroupName -Name $AccountName -Location 'East US 2' -Plan Free -Verbose
 $RegistrationInfo = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName $ResourceGroupName -AutomationAccountName $AccountName
-New-AzureRmResourceGroupDeployment -Name TestDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -registrationKey ($RegistrationInfo.PrimaryKey | ConvertTo-SecureString -AsPlainText -Force) -registrationUrl($RegistrationInfo.Endpoint | ConvertTo-SecureString -AsPlainText -Force) -automationAccountName $AccountName -jobId (New-GUID) -adminUsername $credential.UserName -adminPassword $credential.Password -Verbose
+New-AzureRmResourceGroupDeployment -Name TestDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json -registrationKey ($RegistrationInfo.PrimaryKey | ConvertTo-SecureString -AsPlainText -Force) -registrationUrl($RegistrationInfo.Endpoint | ConvertTo-SecureString -AsPlainText -Force) -automationAccountName $AccountName -jobId (New-GUID) -adminUsername $credential.UserName -adminPassword $credential.Password -domainNameLabel $DomainNameLabel -Verbose
